7e556f84fe275770b8dde0c1830ee0f626a00018
rvattipu <rvattipu@cisco.com>
2022-05-24 21:02:42 +0530

    first commit

---
+++ src/clyfinproject/PersonClafyn.java
@@ -0,0 +1,15 @@
+package clyfinproject;
+
+public class PersonClafyn {
+	public int add(int a,int b) {
+		return a+b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PersonClafyn p = new PersonClafyn();
+		int res = p.add(1,2);
+		System.out.println(res);
+	}
+
+}
