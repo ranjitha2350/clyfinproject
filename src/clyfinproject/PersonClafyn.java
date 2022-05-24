296ff42c5868174d6af2c7342f3318c87e63f13b
rvattipu <rvattipu@cisco.com>
2022-05-24 21:38:57 +0530

    child commit1

--- src/clyfinproject/PersonClafyn.java
+++ src/clyfinproject/PersonClafyn.java
@@ -4,12 +4,17 @@ public class PersonClafyn {
 	public int add(int a,int b) {
 		return a+b;
 	}
+	public int sub(int a,int b) {
+		return a-b;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PersonClafyn p = new PersonClafyn();
 		int res = p.add(1,2);
 		System.out.println(res);
+		int res2 = p.sub(10,5);
+		System.out.println(res2);
 	}
 
 }
